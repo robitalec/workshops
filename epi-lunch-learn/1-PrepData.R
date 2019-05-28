94d4bebc1f69519ff00a99d4e6d7fb46cf657baf
Alec Robitaille <robit.alec@gmail.com>
2019-05-28 15:29:29 -0700

    use janitor

--- epi-lunch-learn/1-PrepData.R
+++ epi-lunch-learn/1-PrepData.R
@@ -1,21 +1,26 @@
 ### Prep
 
 ## Packages
-pkgs <- c('readxl', 'tidyr', 'dplyr')
+pkgs <- c('readxl', 'tidyr', 'dplyr', 'janitor')
 p <- lapply(pkgs, library, character.only = TRUE)
 
 ## Data
 # TODO: copy+paste the whole filepath and delete everything to root
 DT <- read_excel('data/raw-data/ED visits for Alec.xls')
 
+DT
+
+## Cleanup
+# Remove spaces from column names, inconsistent column name formatting
+DT <- DT %>%
+	clean_names(case = 'upper_camel')
+
+# Fill missing information in all columns except Diagnosis and DiagnosisLongText
+DT <- DT %>%
+	fill(-Diagnosis, -DiagnosisLongText)
+
+
+
 ## Processing
-
-
-# TODO: take out spaces from column names
 DT %>%
-	fill(`Chart number`,
-			 YTResidenceCode,
-			 PatientAge,
-			 Gender)
-
-
+	tabyl(Diagnosis, Gender)
