c4e71cf1851bf5bb222c11ce2ff062ec758a5ac7
Alec Robitaille <robit.alec@gmail.com>
2019-05-29 11:35:19 -0700

    fin plots

--- epi-lunch-learn/R/4-Plots.R
+++ epi-lunch-learn/R/4-Plots.R
@@ -5,9 +5,25 @@
 
 
 ### Packages ----
+pkgs <- c('tidyr', 'dplyr', 'ggplot2')
+p <- lapply(pkgs, library, character.only = TRUE)
 
 
 ### Data ----
+DT <- readRDS('data/derived-data/1-prep/cleaned-ed-visits.Rds')
 
 
-### Processing ----
+### Filter ----
+# Select specific diagnoses, e.g. alcohol specific
+alcDT <- DT %>%
+	filter(grepl('alcohol', DiagnosisLongText))
+
+
+### Plots ----
+ggplot(alcDT) +
+	geom_histogram(aes(Diagnosis, group = Gender, fill = Gender), stat = 'count')
+
+
+ggplot(alcDT) +
+	geom_histogram(aes(Diagnosis), stat = 'count') +
+	facet_wrap(~Gender)
