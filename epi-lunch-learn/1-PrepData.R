4cafc22defc404d4bf8ab4191a9be8a52b1d0ee9
Alec Robitaille <robit.alec@gmail.com>
2019-05-28 20:18:36 -0700

    more todos

--- epi-lunch-learn/1-PrepData.R
+++ epi-lunch-learn/1-PrepData.R
@@ -1,7 +1,7 @@
 ### Prep
 
 ## Packages
-pkgs <- c('readxl', 'tidyr', 'dplyr', 'janitor')
+pkgs <- c('readxl', 'tidyr', 'dplyr', 'janitor', 'ggplot2')
 p <- lapply(pkgs, library, character.only = TRUE)
 
 ## Data
@@ -17,13 +17,35 @@ DT <- DT %>%
 
 # Fill missing information in all columns except Diagnosis and DiagnosisLongText
 DT <- DT %>%
-	fill(-Diagnosis, -DiagnosisLongText)
+	fill(-DiagnosisType, -Diagnosis, -DiagnosisLongText)
 
 
 
 ## Processing
-DT %>%
-	tabyl(Diagnosis, Gender)
+# TODO: Count by DiagnosisType = M and alcohol vs drug etc
+# TODO: trends by age, location, day of week etc
+# TODO: multiple drug uses, or shared/common diagnosis often found together
+
+# Counts
+tally
+count
+tabyl
+
+
+
+## Pretend
+selDiag <- c('F100', 'F101')
+
+# OR
+alc <- unique(DT$DiagnosisLongText[grepl('alcohol', DT$DiagnosisLongText, ignore.case = TRUE)])
+coc <- unique(DT$DiagnosisLongText[grepl('cocaine', DT$DiagnosisLongText, ignore.case = TRUE)])
+
+alcDT <- DT %>%
+	filter(DiagnosisLongText %in% alc)
+
+cocDT <- DT %>%
+	filter(DiagnosisLongText %in% coc)
+
 ### Visualize
 ggplot(alcDT) +
 	geom_histogram(aes(Diagnosis, group = Gender, fill = Gender), stat = 'count')
