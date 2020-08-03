fb1698e2e1bd0129969ad38383411ded832e2c84
Alec L. Robitaille <robit.alec@gmail.com>
2020-08-03 13:04:48 -0230

    fin download data

--- some-reproducibility/example/R/1-download-data.R
+++ some-reproducibility/example/R/1-download-data.R
@@ -2,17 +2,20 @@
 # Alec Robitaille ---------------------------------------------------------
 
 
-
 # Packages ----------------------------------------------------------------
-
+library(spatsoc)
+library(data.table)
 
 
 # Input -------------------------------------------------------------------
+# Read the example data from spatsoc
+DT <- fread(system.file("extdata", "DT.csv", package = "spatsoc"))
 
 
-# Process -----------------------------------------------------------------
+# This might alternatively be downloading data from the internet,
+#   reading in a bunch of local files, etc.
 
 
 # Output ------------------------------------------------------------------
-
-
+# Save the data in the input folder
+fwrite(DT, 'input/1-downloaded-data.csv')
