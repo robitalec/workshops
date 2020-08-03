40d10909e007efafbdeadc42c68fc27a056deb75
Alec L. Robitaille <robit.alec@gmail.com>
2020-08-03 13:51:12 -0230

    the plan

--- some-reproducibility/example/scripts/plan.R
+++ some-reproducibility/example/scripts/plan.R
@@ -1,21 +1,29 @@
+# Load drake
 library(drake)
 
-# Source variables
-source('scripts/variables.R')
+# Code to function (for sequential scripts)
+download_data <- code_to_function('scripts/1-download-data.R')
+prepare_data <- code_to_function('scripts/2-prep-data.R')
+process_data <- code_to_function('scripts/3-process.R')
+summarize_data <- code_to_function('scripts/4-summary.R')
+plot_data <- code_to_function('scripts/5-plot.R')
 
-# Set renv option for auto snapshots
-options(renv.config.auto.snapshot = TRUE)
+# Details: https://books.ropensci.org/drake/scripts.html
+# Though drake developer's thoughts: https://books.ropensci.org/drake/scripts.html#final-thoughts
 
 
-data_prep <- code_to_function('scripts/data-prep.R')
-process <- code_to_function('scripts/process.R')
-
+# The plan
 plan <- drake_plan(
-  prepared = data_prep(),
-  processed = process(prepared),
-  report =   rmarkdown::render(
-    knitr_in('md/report.Rmd'),
-    output_format = 'all',
-    quiet = TRUE
-  )
+  downloaded = download_data(),
+  prepared = prep_data(downloaded),
+  processed = process_data(prepared),
+  summarized = summarize_data(processed),
+  plotted = plot_data(summarized),
 )
+
+# Plans can extend in many ways:
+# * multiple dependencies
+# * non linear dependencies
+# * knitr/markdown reports
+# * etc etc etc
+
