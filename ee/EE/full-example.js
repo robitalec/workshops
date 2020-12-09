eb4f9f3f13136503a9c644afabd54a0f8ce54c8f
icrichmond <icrichmond@mun.ca>
2020-12-09 11:32:55 -0500

    Added commenting

--- ee/EE/full-example.js
+++ ee/EE/full-example.js
@@ -1,4 +1,9 @@
-// functions
+// Full worked example
+
+// First we are going to build some functions
+// Earth Engine has several functions ready to go in the example scripts
+// Functions are often dependent on the image collection you are working with
+
 // Function to cloud mask from the pixel_qa band of Landsat 8 SR data.
 function maskL8sr(image) {
 	// Bits 3 and 5 are cloud shadow and cloud, respectively.
@@ -19,20 +24,25 @@ function maskL8sr(image) {
 }
 
 
-//function to calculate NDVI and add it as a band
+//Function to calculate NDVI and add it as a band
 function calcNDVI(image) {
 	var ndvi = image.normalizedDifference(['B4','B3']).rename('ndvi')
 	return image.addBands(ndvi);
 }
 
-
-// import
-
+// Now we are going to import the image collection that we want
+// We can import using the image collection ID, as seen below
+// To find an Image Collection's ID, search for the Image Collection you want
+// in the search bar, click on it, and look in the description
 var l8 = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR");
 
 
-// filter
-
+// We want to filter the image collection to our area and time of interest
+// Use the "Draw a Rectangle" tool in the bottom left to draw a box around your "study area"
+// .filterDate will choose the images in the image collection that are within our specified timeframe
+// .filterBounds will choose the images in the image collection that overlap with our study area
+// We are also going to use our cloud masking and NDVI functions
+// Limit provides a max number of images to be used
 l8 = l8.filterDate('2018-01-01', '2019-01-01')
 .filterBounds(geometry)
 .map(maskL8sr)
@@ -40,19 +50,29 @@ l8 = l8.filterDate('2018-01-01', '2019-01-01')
 .limit(7);
 
 
-// sample
+// Here, we are creating another function. This function will sample our collection
+// After sampling, it will calculate the mean at the scale that you specify (30)
 var sampleregions = function(im) {
 	return(im.reduceRegions(geometry,
 													ee.Reducer.mean(),
 													30));
 };
 
-
+// Here, we use the function to sample our collection
 var sample = l8.map(sampleregions);
 
+// The print function causes our collection to pop up in the console panel on the right
 print(sample)
+
 // chart + map
 
-Map.addLayer(l8.limit(2), imageVisParam);
+// Add the processed layers to the map below to visualize your data
+Map.addLayer(l8.limit(2));
+Map.addLayer(l8.select('ndvi'))
 
+// After adding layers, you can use the "Layers" tool on the bottom right to adjust
+// the visualization parameters to make them more appropriate - then import them
+// The imported visualization parameters will appear as imageVisParam
+// You can do it for both layers
+Map.addLayer(l8.limit(2), imageVisParam);
 Map.addLayer(l8.select('ndvi'), imageVisParam2)
