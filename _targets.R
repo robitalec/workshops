801011d6f1877f66ad114f8591bc0bdc594d91a6
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-26 14:25:39 -0800

    find extras and copy them to docs

--- _targets.R
+++ _targets.R
@@ -11,6 +11,13 @@ find_rmds <- function(dirs) {
 	vapply(drop_rprojuser, dir, pattern = '.Rmd$', full.names = TRUE, 'potato')
 }
 
+find_extras <- function(dirs) {
+	basename_dirs <- lapply(dirs, basename)
+	slides_dirs <- dirs[grep('slides$', basename_dirs)]
+	drop_rprojuser <- grep('.Rproj.user', slides_dirs, invert = TRUE, value = TRUE)
+	unlist(lapply(drop_rprojuser, dir, pattern = 'css$|js$', full.names = TRUE))
+}
+
 c(
 	tar_target(
 		dirs,
@@ -19,6 +26,16 @@ c(
 	tar_target(
 		rmds,
 		find_rmds(dirs)
+	),
+	tar_target(
+		extras,
+		find_extras(dirs)
+	),
+	tar_target(
+		extra,
+		extras,
+		pattern = map(extras),
+		format = 'file'
 	),
 	tar_target(
 		rmd,
@@ -33,5 +50,10 @@ c(
 			output_dir = 'docs'
 		),
 		pattern = map(rmd)
+	),
+	tar_target(
+		copy_extras,
+		file.copy(extra, 'docs', overwrite = TRUE),
+		pattern = map(extra)
 	)
 )
