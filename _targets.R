14e4a177712290aceb7f647e0ae36b2ba7d78cb9
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 09:08:54 -0700

    add targets find images and mv to docs/images/

--- _targets.R
+++ _targets.R
@@ -18,25 +18,38 @@ find_extras <- function(dirs) {
 	unlist(lapply(drop_rprojuser, dir, pattern = 'css$|js$', full.names = TRUE))
 }
 
+find_images <- function(dirs) {
+	paths_slides <- dir(dirs, pattern = 'slides', include.dirs = TRUE, recursive = TRUE)
+	paths_slides_images <- lapply(paths_slides, dir, 'images', full.names = TRUE)
+	unlist(paths_slides_images)
+}
+
 c(
 	tar_target(
 		dirs,
-		list.dirs('.')
+		list.dirs('.'),
+		format = 'file'
 	),
 	tar_target(
 		rmds,
 		find_rmds(dirs)
 	),
 	tar_target(
-		extras,
-		find_extras(dirs)
-	),
-	tar_target(
 		extra,
-		extras,
-		pattern = map(extras),
+		find_extras(dirs),
 		format = 'file'
 	),
+	tar_target(
+		imgs,
+		find_images(dirs),
+		format = 'file'
+	),
+	# tar_target(
+	# 	extra,
+	# 	extras,
+	# 	pattern = map(extras),
+	# 	format = 'file'
+	# ),
 	tar_target(
 		rmd,
 		rmds,
@@ -53,8 +66,11 @@ c(
 	),
 	tar_target(
 		copy_extras,
-		file.copy(extra, 'docs', overwrite = TRUE),
-		pattern = map(extra)
+		file.copy(extra, 'docs', overwrite = TRUE)
+	),
+	tar_target(
+		copy_imgs,
+		file.copy(dir(imgs, full.names = TRUE), file.path('docs', 'images'))
 	),
 	tar_target(
 		workshops_csv,
