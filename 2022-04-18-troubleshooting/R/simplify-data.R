474432a8697155500e03cd0a069c72c86522dff8
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-16 17:21:26 -0700

    fst simplify-data

---
+++ 2022-04-18-troubleshooting/R/simplify-data.R
@@ -0,0 +1,33 @@
+# Simplify: data ----------------------------------------------------------
+# Alec L. Robitaille
+
+
+
+# Packages ----------------------------------------------------------------
+
+# (Extras)
+library(charlatan)
+
+
+
+
+# Helpers -----------------------------------------------------------------
+# Set an consistent number to sample
+N <- 10
+
+
+
+# UPPERCASE and lowercase letters in A-Z
+LETTERS
+letters
+
+# Sequence of letters from 1-N
+LETTERS[seq.int(N)]
+letters[seq.int(N)]
+
+# Sample N letters
+sample(LETTERS, N)
+sample(letters, N)
+
+
+
