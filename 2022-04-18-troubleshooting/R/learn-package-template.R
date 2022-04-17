c0770ef1eef523d4d2e7be6db68676c7c934eba2
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-16 17:30:05 -0700

    fst learn package template

---
+++ 2022-04-18-troubleshooting/R/learn-package-template.R
@@ -0,0 +1,36 @@
+# === Learn {package} -----------------------------------------------------
+# Alec L. Robitaille
+
+
+
+# Install -----------------------------------------------------------------
+# install.packages('package')
+
+
+
+# Packages ----------------------------------------------------------------
+# library(package)
+
+# For data, if suitable
+# library(palmerpenguins)
+
+library(data.table)
+
+
+
+# Data --------------------------------------------------------------------
+DT <- data.table(penguins)
+
+
+
+
+# Try {package} -----------------------------------------------------------
+# Function 1
+
+
+# Function 2
+
+
+# Function 3
+
+
